af3b89d2a099f3fd3f85ee15030dfd460d5c58eb
Abdul Sameeu <abdulsameeu26@gmail.com>
2024-08-26 16:56:24 +0530

    Add Service Test,Controller Test

--- src/test/java/com/ust/elkstackdemo/service/ContactServiceTest.java
+++ src/test/java/com/ust/elkstackdemo/service/ContactServiceTest.java
@@ -9,12 +9,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class ContactServiceTest {
 
@@ -26,6 +27,8 @@ class ContactServiceTest {
      * 2. addContact_WithNullName_ShouldThrowException
      * 3. addContact_WithEmptyName_ShouldThrowException
      * 4. addContact_WithNullPhoneNumber_ShouldThrowException
+     *
+     *
      * 5. addContact_WithEmptyPhoneNumber_ShouldThrowException
      * 6. addContact_WithExistingContact_ShouldThrowException
      */
@@ -38,11 +41,13 @@ class ContactServiceTest {
 
     List<Contact> contacts;
     Contact contact;
+    Contact nullname;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
         contact = new Contact("John Doe", "john.doe", "1234567890");
+        nullname = new Contact(null, "john.doe", "1234567890");
         contacts = List.of(contact);
     }
 
@@ -54,6 +59,68 @@ class ContactServiceTest {
         verify(contactRepository).addContact(any(Contact.class));
         assertEquals(1, contactRepository.getContacts().size());
     }
+
+    @Test
+    @DisplayName("Add contact with null name should throw exception")
+    void addContact_WithNullName_ShouldThrowException() {
+        doThrow(new IllegalArgumentException("Contact name is empty"))
+                .when(contactRepository)
+                .addContact(any(Contact.class));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                contactService.addContact(null, "john.doe@example.com", "1234567890")
+        );
+        assertEquals("Contact name is empty", exception.getMessage());
+    }
+    @Test
+    @DisplayName("Add contact with empty name should throw exception")
+    void addContact_WithEmptyName_ShouldThrowException() {
+        doThrow(new IllegalArgumentException("Contact name is empty"))
+                .when(contactRepository)
+                .addContact(any(Contact.class));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                contactService.addContact("", "john.doe@example.com", "1234567890")
+        );
+        assertEquals("Contact name is empty", exception.getMessage());
+    }
+    @Test
+    @DisplayName("Add contact with null number should throw exception")
+    void addContact_WithNullPhoneNumber_ShouldThrowException() {
+        doThrow(new IllegalArgumentException("Contact number is empty"))
+                .when(contactRepository)
+                .addContact(any(Contact.class));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                contactService.addContact("Jane Doe", "john.doe@example.com", null)
+        );
+        assertEquals("Contact number is empty", exception.getMessage());
+    }
+    @Test
+    @DisplayName("Add contact with empty number should throw exception")
+    void addContact_WithEmptyPhoneNumber_ShouldThrowException() {
+        doThrow(new IllegalArgumentException("Contact number is empty"))
+                .when(contactRepository)
+                .addContact(any(Contact.class));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                contactService.addContact("Jane Doe", "john.doe@example.com", "1234567890")
+        );
+        assertEquals("Contact number is empty", exception.getMessage());
+    }
+    @Test
+    @DisplayName("Add contact with existing contact should throw exception")
+    void addContact_WithExistingContact_ShouldThrowException() {
+        // Arrange
+        // Mock the behavior of contactRepository to throw an exception when addContact is called
+        doThrow(new IllegalArgumentException("Contact already exists"))
+                .when(contactRepository)
+                .addContact(any(Contact.class));
+
+        // Use assertThrows to check if the correct exception is thrown
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                {
+                    contactService.addContact(contact.getName(), contact.getEmail(), contact.getPhoneNumber());
+                });
+        assertEquals("Contact already exists", exception.getMessage());
+    }
+
 
 
 }
